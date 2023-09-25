b3c61e5ab87517a9e2f677a5749e2fe7a98d95ff
dlakshmeek <145945199+dlakshmeek@users.noreply.github.com>
2023-09-25 09:06:16 +0800

    Add files via upload

---
+++ day2/Firefox.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     float browserVersion = 100.2f;
+     String browserName = "firefox";
+     boolean isVisible = true;
+     int releaseYear = 1998;
+     char browserLogo ='f';
+     
+     System.out.println("My browser Version is" + "=" + browserVersion);
+     System.out.println("My browser Name is" + "=" + browserName); 
+     System.out.println("My browser visibility status"+ "=" + isVisible);
+     System.out.println("My release year " + "=" + releaseYear);
+     System.out.println("My browserLogo is " + "=" + browserLogo);
+
+}
+}
---
+++ day2/OddNumber.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class OddNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The odd numbers are");
+   for (int i = 2; i <=20; i++) {
+	if (i % 2 ==1) {
+		
+		System.out.println(i);
+	}
+}
+	}
+
+}
