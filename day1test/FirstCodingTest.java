095aa69fe7ba6fe36635156fbfdfcc0e78a1dd68
dlakshmeek <145945199+dlakshmeek@users.noreply.github.com>
2023-09-25 09:05:29 +0800

    Add files via upload

---
+++ day1test/FirstCodingTest.java
@@ -0,0 +1,11 @@
+package week1.day1test;
+
+public class FirstCodingTest {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the new project");
+		System.out.println("we are going to rock");
+
+	}
+
+}
